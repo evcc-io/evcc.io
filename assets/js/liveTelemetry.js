bf210a2a009bd9ee059df98092d177b0313804bb
Michael Geers <michael@geers.tv>
2022-10-10 17:16:26 +0200

    remove initial counting animation; error handling

--- assets/js/liveTelemetry.js
+++ assets/js/liveTelemetry.js
@@ -2,35 +2,40 @@ import { CountUp } from "./vendor/countUp";
 
 const UPDATE_INTERVAL_SECONDS = 5;
 
-const countOptions = {
-  duration: UPDATE_INTERVAL_SECONDS,
-  useEasing: false,
-  separator: ".",
-  decimal: ",",
-  enableScrollSpy: true,
-  scrollSpyOnce: true,
-};
+function createCounter(id, value, decimalPlaces) {
+  return new CountUp(id, value, {
+    startVal: value,
+    duration: UPDATE_INTERVAL_SECONDS,
+    useEasing: false,
+    separator: ".",
+    decimal: ",",
+    decimalPlaces,
+  });
+}
 
-const powerCount = new CountUp("telemetry--power", 0, {
-  ...countOptions,
-  decimalPlaces: 1,
-});
-const greenPowerCount = new CountUp("telemetry--greenPower", 0, {
-  ...countOptions,
-  decimalPlaces: 1,
-});
-const greenEnergyCount = new CountUp("telemetry--greenEnergy", 0, {
-  ...countOptions,
-  decimalPlaces: 2,
-});
+let powerCount = null;
+let greenPowerCount = null;
+let greenEnergyCount = null;
 
 const $activeClients = document.querySelector("#telemetry--activeClients");
 const $totalClients = document.querySelector("#telemetry--totalClients");
 
 function render(data) {
-  powerCount.update(data.chargePower / 1e3);
-  greenPowerCount.update((100 / data.chargePower) * data.greenPower);
-  greenEnergyCount.update(data.greenEnergy / 1e3);
+  document.querySelector(".telemetry").style.display = "block";
+
+  const chargePower = data.chargePower / 1e3;
+  const greenPower = (100 / data.chargePower) * data.greenPower;
+  const greenEnergy = data.greenEnergy / 1e3;
+
+  if (!powerCount) {
+    powerCount = createCounter("telemetry--power", chargePower, 1);
+    greenPowerCount = createCounter("telemetry--greenPower", greenPower, 1);
+    greenEnergyCount = createCounter("telemetry--greenEnergy", greenEnergy, 2);
+  } else {
+    powerCount.update(chargePower);
+    greenPowerCount.update(greenPower);
+    greenEnergyCount.update(greenEnergy);
+  }
   $activeClients.innerText = data.activeClients;
   $totalClients.innerText = data.totalClients;
 }
@@ -38,7 +43,11 @@ function render(data) {
 function update() {
   fetch("https://api.evcc.io/v1/total")
     .then((response) => response.json())
-    .then(render);
+    .then(render)
+    .catch((err) => {
+      console.error(err);
+      document.querySelector(".telemetry").style.display = "none";
+    });
 }
 
 setInterval(update, UPDATE_INTERVAL_SECONDS * 1e3);
