5f828a17ec855f4ecf8e8acac77a328659855734
naltatis <naltatis@users.noreply.github.com>
2024-11-15 22:55:27 +0000

    deploy: 34ed814e83fa6dfbc0c9d3fb4c1493faf4cc3d3e

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@
     $html.classList.toggle("dark", isDark);
   }
   var darkModeMatcher = window.matchMedia("(prefers-color-scheme: dark)");
-  darkModeMatcher.addEventListener("change", () => updateTheme(darkModeMatcher.matches));
+  darkModeMatcher.addEventListener(
+    "change",
+    () => updateTheme(darkModeMatcher.matches)
+  );
   updateTheme(darkModeMatcher.matches);
   document.querySelectorAll("[data-theme]").forEach((el) => {
     el.addEventListener("click", (e) => {
@@ -16,10 +19,11 @@
   document.querySelectorAll("a[href^='./#'], a[href^='../en/#']").forEach((el) => {
     el.addEventListener("click", (e) => {
       e.preventDefault();
-      const target = document.getElementById(el.getAttribute("href").split("#").pop());
+      const target = document.getElementById(
+        el.getAttribute("href").split("#").pop()
+      );
       console.log(el.getAttribute("href").split("#").pop());
-      if (!target)
-        return;
+      if (!target) return;
       target.scrollIntoView({ behavior: "smooth" });
     });
   });
