8a22b5c7ddad49e4a8361e9fce6305069e07d7b0
Michael Geers <michael@geers.tv>
2026-04-01 17:24:12 +0200

    fix

--- assets/js/liveTelemetry.test.js
+++ assets/js/liveTelemetry.test.js
@@ -1,5 +1,5 @@
 import { expect, it } from "vitest";
-import { fmtKW, fmtKWh } from "./liveTelemetry";
+import { fmtKW } from "./liveTelemetry";
 
 it("fmtKW", () => {
   expect(fmtKW(1)).toEqual({ value: 1, unit: "kW", decimals: 0 });
@@ -31,36 +31,3 @@ it("fmtKW", () => {
   });
 });
 
-it("fmtKWh", () => {
-  expect(fmtKWh(1)).toEqual({ value: 1, unit: "kWh", decimals: 0 });
-  expect(fmtKWh(12)).toEqual({ value: 12, unit: "kWh", decimals: 0 });
-  expect(fmtKWh(123)).toEqual({ value: 123, unit: "kWh", decimals: 0 });
-  expect(fmtKWh(1234)).toEqual({ value: 1.234, unit: "MWh", decimals: 2 });
-  expect(fmtKWh(12345)).toEqual({ value: 12.345, unit: "MWh", decimals: 1 });
-  expect(fmtKWh(123456)).toEqual({ value: 123.456, unit: "MWh", decimals: 0 });
-  expect(fmtKWh(1234567)).toEqual({
-    value: 1.234567,
-    unit: "GWh",
-    decimals: 2,
-  });
-  expect(fmtKWh(12345678)).toEqual({
-    value: 12.345678,
-    unit: "GWh",
-    decimals: 1,
-  });
-  expect(fmtKWh(123456789)).toEqual({
-    value: 123.456789,
-    unit: "GWh",
-    decimals: 0,
-  });
-  expect(fmtKWh(1234567890)).toEqual({
-    value: 1234.56789,
-    unit: "GWh",
-    decimals: 0,
-  });
-  expect(fmtKWh(12345678901)).toEqual({
-    value: 12345.678901,
-    unit: "GWh",
-    decimals: 0,
-  });
-});
