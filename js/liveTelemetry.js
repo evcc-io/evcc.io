a4eaf47d5390af4670eb9750a4839dba9ea8e620
naltatis <naltatis@users.noreply.github.com>
2026-04-01 15:30:34 +0000

    deploy: 696c3844ca445999174b3fbe4af9c2c9e11ae684

--- js/liveTelemetry.js
+++ js/liveTelemetry.js
@@ -268,25 +268,20 @@
   }
   var powerCount = null;
   var greenShareCount = null;
-  var greenEnergyCount = null;
   var $activeClients;
   var $totalClients;
   var $chargePowerUnit;
-  var $unitGreenEnergy;
   function initializeElements() {
     $activeClients = document.querySelector("#telemetry--activeClients");
     $totalClients = document.querySelector("#telemetry--totalClients");
     $chargePowerUnit = document.querySelector("#telemetry--chargePowerUnit");
-    $unitGreenEnergy = document.querySelector("#telemetry--greenEnergyUnit");
   }
   function render(data) {
     const telemetryElement = document.querySelector(".telemetry");
     if (telemetryElement) {
       telemetryElement.style.display = "block";
     }
-    data.greenEnergy = 55123456;
     const chargePower = fmtKW(data.chargePower / 1e3);
-    const greenEnergy = fmtKWh(data.greenEnergy);
     const greenShare = 100 / data.chargePower * data.greenPower;
     if (!powerCount) {
       powerCount = createCounter(
@@ -294,21 +289,18 @@
         chargePower.value,
         chargePower.decimals
       );
-      greenEnergyCount = createCounter(
-        "telemetry--greenEnergy",
-        greenEnergy.value,
-        greenEnergy.decimals
-      );
       greenShareCount = createCounter("telemetry--greenShare", greenShare, 1);
     } else {
       powerCount.update(chargePower.value);
-      greenEnergyCount.update(greenEnergy.value);
       greenShareCount.update(greenShare);
     }
     $chargePowerUnit.innerText = chargePower.unit;
-    $unitGreenEnergy.innerText = greenEnergy.unit;
-    $activeClients.innerText = data.activeClients;
-    $totalClients.innerText = data.totalClients;
+    const lang = document.documentElement.lang || "de";
+    const fmt = new Intl.NumberFormat(lang);
+    $activeClients.innerText = fmt.format(data.activeClients);
+    if ($totalClients) {
+      $totalClients.innerText = fmt.format(data.totalClients);
+    }
   }
   function update() {
     fetch("https://api.evcc.io/v1/total").then((response) => response.json()).then(render).catch((err) => {
@@ -334,11 +326,6 @@
       return { value: kW / 1e6, decimals: 1, unit: "GW" };
     }
     return { value: kW / 1e6, decimals: 0, unit: "GW" };
-  }
-  function fmtKWh(kWh) {
-    const result = fmtKW(kWh);
-    result.unit += "h";
-    return result;
   }
   if (typeof window !== "undefined" && !window.vitest) {
     initializeElements();
